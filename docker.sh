bec74784f0164fee420b1ae53541dc57c63a6300
Siddhartha <61656756+SiddharthaShandilya@users.noreply.github.com>
2021-06-27 08:36:07 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,7 @@
-sudo cat <<EOF >  /etc/yum.repos.d/docker.repo
-[docker]
-name=docker
-baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/
-gpgcheck=0
-EOF
-
-sudo yum install docker-ce --nobest -y
+sudo yum install docker -y
 
 sudo systemctl restart docker
 
 sudo docker ps
+
+docker run -dit httpd
